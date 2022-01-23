d79fe6c239f4713777fd033fc63a1f7d2bcfe6db
Harris Schneiderman <schne324@gmail.com>
2022-01-23 13:23:54 -0800

    Update recipes.js

--- src/recipes.js
+++ src/recipes.js
@@ -304,5 +304,29 @@ export default [
       'Remove the chicken breasts from the pot and place on a plate or cutting board. Shred the chicken using two forks.',
       'Remove 1/2 cup of liquid from the pot. (Reserve the liquid for cooking rice.) Return the shredded chicken to pot and stir until well combined. Top chicken with green onions.'
     ]
-  }
+  },
+  {
+    name: 'Pulled pork',
+    tags: ['slow cooker', 'bbq'],
+    ingredients: [
+      '1 teaspoon vegetable oil',
+      '1 (4 pound) pork shoulder roast',
+      '1 cup barbeque sauce',
+      '1/2 cup apple cider vinegar',
+      '1/2 cup chicken broth',
+      '1/4 cup light brown sugar',
+      '1 tablespoon prepared yellow mustard',
+      '1 tablespoon Worcestershire sauce',
+      '1 tablespoon chili powder',
+      '1 extra large onion, chopped',
+      '2 large cloves garlic, crushed',
+      '1 1⁄2 teaspoons dried thyme',
+      '8 hamburger buns, split',
+      '2 tablespoons butter, or as needed'
+    ],
+    instructions: [
+      'Pour the vegetable oil into the bottom of a slow cooker. Place the pork roast into the slow cooker; pour in the barbecue sauce, apple cider vinegar, and chicken broth. Stir in the brown sugar, yellow mustard, Worcestershire sauce, chili powder, onion, garlic, and thyme. Cover and cook on High until the roast shreds easily with a fork, 5 to 6 hours.',
+      'Remove the roast from the slow cooker, and shred the meat using two forks. Return the shredded pork to the slow cooker, and stir the meat into the juices.',
+      'Spread the inside of both halves of hamburger buns with butter. Toast the buns, butter side down, in a skillet over medium heat until golden brown. Spoon pork into the toasted buns.'
+    ]
 ];
